ea55ce128cb9d2f568cc1ef9130abed4038b3e42
rishikeshpawar1312 <rishikeshpawar210492@acropolis.in>
2025-01-18 23:42:19 +0530

    added skills in skills section

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import './Skill.css'
 import clang from '../../assests/clang.png'
 import clogo from '../../assests/clogo.png'
@@ -12,8 +11,15 @@ import tlogo from '../../assests/tlogo.png'
 import reduxlogo from '../../assests/reduxlogo.png'
 import nodelogo from '../../assests/nodelogo.png'
 import mysqllogo from '../../assests/mysqllogo.png'
+import  typescript from '../../assests/typescript.png'
+import  reactnative from '../../assests/reactnative.png'
+import  appwrite from '../../assests/appwrite.png'
+import  mongodb from '../../assests/mongodb.png'
+import   Nextjs from '../../assests/Nextjs.png'
+import    cloudinary from '../../assests/cloudinary.svg'
 
- 
+
+
 
 function Skill() {
   return (
@@ -22,38 +28,28 @@ function Skill() {
         <div className="skillstitle">Skills</div>
 
         <div className="skills-category">
-          <div className="skills-category-title">Frontend Development</div>
           <div className="skillsimage">
             <img src={hlogo} alt="HTML" className="skillsimg" title="HTML" />
             <img src={csslogo} alt="CSS" className="skillsimg" title="CSS" />
             <img src={jlogo} alt="JavaScript" className="skillsimg" title="JavaScript" />
             <img src={rlogo} alt="React" className="skillsimg" title="React" />
             <img src={reduxlogo} alt="Redux" className="skillsimg" title="Redux" />
-          </div>
-        </div>
-
-        <div className="skills-category">
-          <div className="skills-category-title">Backend Development</div>
-          <div className="skillsimage">
             <img src={nodelogo} alt="Node.js" className="skillsimg" title="Node.js" />
             <img src={mysqllogo} alt="MySQL" className="skillsimg" title="MySQL" />
-          </div>
-        </div>
-
-        <div className="skills-category">
-          <div className="skills-category-title">Programming Languages</div>
-          <div className="skillsimage">
             <img src={clang} alt="C" className="skillsimg" title="C" />
             <img src={clogo} alt="C++" className="skillsimg" title="C++" />
-          </div>
-        </div>
-
-        <div className="skills-category">
-          <div className="skills-category-title">Tools & Platforms</div>
-          <div className="skillsimage">
             <img src={glogo} alt="GitHub" className="skillsimg" title="GitHub" />
             <img src={blogo} alt="Bootstrap" className="skillsimg" title="Bootstrap" />
             <img src={tlogo} alt="Tailwind CSS" className="skillsimg" title="Tailwind CSS" />
+            <img src={typescript} alt="Typescript" className="skillsimg" title="Typrscript" />
+            <img src={reactnative} alt="React Native" className="skillsimg" title="React Native" />
+            <img src={mongodb} alt="Mongodb" className="skillsimg" title="Mongodb" />
+            <img src={appwrite} alt="Appwrite" className="skillsimg" title="Appwrite" />
+            <img src={Nextjs} alt="Nextjs" className="skillsimg" title="Nextjs" />
+            <img src={cloudinary} alt="Cloudinary" className="skillsimg" title="Cloudinary" />
+
+
+
           </div>
         </div>
       </div>
@@ -62,4 +58,3 @@ function Skill() {
 }
 
 export default Skill;
-
